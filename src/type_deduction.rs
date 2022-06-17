80b580d4a224592849683e54e7fac8969e2ded2b
MikeFromWarsaw <michal.jelicz@gmail.com>
2022-06-17 12:58:44 +0200

    Update type_deduction.rs

--- src/type_deduction.rs
+++ src/type_deduction.rs
@@ -1,4 +1,7 @@
 fn main() {
+    // Prosty i dobry opis zagadnienia składni turbofish można znaleźć tutaj
+    // https://techblog.tonsser.com/posts/what-is-rusts-turbofish
+       
     let numbers : Vec<i32> = vec![1, 2, 3];
     
     // Use `turbofish` (::<type>) syntax is used to give a hint to the collect
@@ -9,11 +12,28 @@ fn main() {
     // 2 rev() - tworzy i zwraca iterator zwracający elementy w odwrotnej kolejności
     // 3 collect() - umieszcza elementy dostępne iteratorowi w wektorze i go zwraca.
     let result1 = numbers.iter().rev().collect::<Vec<&i32>>();
-    println!("{:?}", result);
+    println!("Odwrócony wektor {:?}", result1);
     
-    // Alternatywnie możemy określić typ zmiennej result2 w momencie jej deklaracji.
+    // Alternatywnie możemy określić typ zmiennej resul2 w momencie jej deklaracji.
     // Dzięki temu collect użyje typu zmiennej result2 w celu utworzenia i zwrócenia
     // wymaganej kolekcji z elementami.
     let result2: Vec<&i32> = numbers.iter().rev().collect();
-    println!("{:?}", result2);
+    println!("Odwrócony wektor {:?}", result2);
+
+    // Znajdź, ile jest szóstek na koniec roku w klasie z danego przedmiotu. 
+    let class_scores : Vec<i32> = vec![5, 3, 3, 3, 2, 6, 6];
+
+    // Przy użyciu collect (nieefektywne) i składni turobfish.
+    let scored  = class_scores
+        .into_iter()
+        .filter(|n| n == &6)
+        .collect::<Vec<i32>>().len();
+
+    // TODO: Przy użyciu funkcji count.
+    // scored  = class_scores
+    //    .into_iter()
+    //    .filter(|n| n == &6)
+    //    .count();
+
+    println!("Liczna 6 w klasie to {:?}", scored);
 }
